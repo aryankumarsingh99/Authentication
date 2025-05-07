d9687af8aa0cb4ed6a7127b2a482693d39e979e6
aryankumarsingh <aryankumarsingh8340@gmail.com>
2025-05-07 19:59:26 +0530

    add dotenv configuration and update .gitignore for environment variables

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,40 @@
 const express = require("express"); //import express
 const mongoose = require("mongoose");
 const cors = require("cors");
-const  UserModel = require("./models/Authentication");
+const UserModel = require("./models/Authentication");
+const dotenv = require("dotenv");
+dotenv.config(); // load env variables
 
 const app = express(); // create app
 app.use(express.json());
 app.use(cors());
 
+// const mongoose = require('mongoose');
+const uri =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Authentication";
+
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
+
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
-  console.log(email, password);                                                       
+  console.log(email, password);
 
   try {
     UserModel.findOne({ email: email }).then((user) => {
@@ -28,8 +53,6 @@ app.post("/login", (req, res) => {
   }
 });
 
-
-
 app.post("/signup", (req, res) => {
   const body = req.body;
   console.log(body);
@@ -39,7 +62,7 @@ app.post("/signup", (req, res) => {
       return res.send({ message: "empty fields", code: 400 });
     }
     UserModel.create(req.body)
-      .then((User) => res.json( User))
+      .then((User) => res.json(User))
       .catch((err) => res.json(err));
 
     return res.send({ message: "success", code: 200 });
@@ -50,6 +73,6 @@ app.post("/signup", (req, res) => {
 });
 
 //Connection mongoose
-mongoose.connect("mongodb://127.0.0.1:27017/Authentication");
+// mongoose.connect("mongodb://127.0.0.1:27017/Authentication");
 
-app.listen(8000, () => console.log("Server is running"));  
+app.listen(8000, () => console.log("Server is running"));
